d1102a147976f1d674980b17e5f38e4444616872
zzl <1158508539@qq.com>
2022-02-19 17:43:23 +0800

    路由匹配和视图

--- bookmange/book/views.py
+++ bookmange/book/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+# request
+from django.http import HttpResponse
+from django.http import HttpRequest
+
+def index(request):
+
+    return HttpResponse('ojbk')
